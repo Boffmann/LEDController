6f4c6199b555b930ccb59596a637e8680c4cbc1e
Hendrik Tjabben <hendrik.tjabben@student.hpi.de>
2019-09-09 13:55:42 +0200

    Set default brightness seekbar value

--- app/src/main/java/com/hendrik/ledcontroller/MainMenu.java
+++ app/src/main/java/com/hendrik/ledcontroller/MainMenu.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -14,7 +13,6 @@ import android.widget.TextView;
 import com.hendrik.ledcontroller.Bluetooth.BTService;
 import com.hendrik.ledcontroller.Bluetooth.Command.BTPackage;
 
-import java.io.Console;
 
 
 public class MainMenu extends BaseBTActivity {
